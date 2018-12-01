fa6ebc011d1ee336b720323fdd9bd383383cfb62
cristian leonardo <cristianleonardo999@gmail.com>
2018-12-01 09:43:59 -0500

    fix bugs in migrations

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,4 @@
 class Person < ApplicationRecord
   has_one :student
   has_one :teacher
-  has_one :student
 end
--- db/migrate/20181128151401_create_student_licence.rb
+++ db/migrate/20181128151401_create_student_licence.rb
@@ -2,12 +2,12 @@ class CreateStudentLicence < ActiveRecord::Migration[5.2]
   def change
     create_table :student_licences do |t|
       t.integer :student_id, null: false
-      t.integer :licence_id, null: false
+      t.integer :licence_type_id, null: false
       t.datetime :expedition_date, null: false
       t.datetime :expiration_date, null: false
       t.timestamps
     end
     add_index :student_licences, :student_id
-    add_index :student_licences, :licence_id
+    add_index :student_licences, :licence_type_id
   end
 end
