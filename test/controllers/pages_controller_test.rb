0567b281f27febf85f93049eb5315f24cece09eb
cristianleonardop <cristianleonardo999@gmail.com>
2018-12-09 20:40:19 -0500

    delete integration test

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get welcome" do
-    get pages_welcome_url
-    assert_response :success
-  end
 
 end
